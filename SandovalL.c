7cf82cabe10999f7c80a269dbd6f374009796254
leebrien <lee_sandoval@dlsu.edu.ph>
2023-03-29 20:26:48 +0800

    M2Start

--- SandovalL.c
+++ SandovalL.c
@@ -62,17 +62,89 @@ void getPassword(String30 password)
     password[i] = '\0';
 }
 
-/* This function is for getting the password of the admin.
+/* This function is for logging into admin after registration
 Precondition: The length of the password is limited to 30 characters
-@param password This holds the inputted string as a password
-@param returnMenu This is for choosing if the user wishes to return to the 
+@param password This holds the inputted string as a password.
+@param currentPassword is used if user will return to manage data.
+@param returnMenu This is for choosing if the user wishes to return to the
        main menu after an incorrect password input.
+@param bContinue is used to signal if login/registration was successful, thus continue through
+       managing the data.
 @return void
 */
-void adminRegistration(String30 password, bool *returnMenu)
+void adminLogin(String30 password, String30 currentPassword, bool *returnMenu, bool *bContinue)
 {
 
-    String30 tempPassword;
+    // To signal validity of input for the whole function.
+    bool validInput = false;
+
+    // To signal validity of input when password did not match.
+    bool validInput2 = false;
+
+    int dInput;
+
+    do
+    {
+        printf("\n=== Admin Login ===\n\n");
+        printf("Enter password: \n");
+        getPassword(password);
+
+        if (strcmp(password, currentPassword) == 0)
+        {
+
+            printf("\n\nLogin Successful!\n\n");
+            system("pause");
+            system("cls");
+
+            validInput = true;
+            *bContinue = true;
+        }
+
+        else if (strcmp(password, currentPassword) != 0)
+        {
+
+            do
+            {
+                system("cls");
+                printf("PASSWORD DO NOT MATCH");
+                printf("\n\nPress [1] to retry");
+                printf("\nPress [2] to return to main menu");
+
+                printf("\nEnter: ");
+                scanf("%d", &dInput);
+
+                if (dInput == 2)
+                {
+                    validInput = true;
+                    validInput2 = true;
+                    *returnMenu = true;
+                }
+
+                else if (dInput == 1)
+                {
+                    validInput2 = true;
+                    system("cls");
+                }
+
+            } while (validInput2 == false);
+        }
+
+    } while (validInput == false);
+}
+
+/* This function is for registering a password for the first time.
+Precondition: The length of the password is limited to 30 characters
+@param password This holds the inputted string as a password.
+@param tempPassword is used to compare the password input.
+@param currentPassword is used if user will return to manage data.
+@param returnMenu This is for choosing if the user wishes to return to the
+       main menu after an incorrect password input.
+@param bContinue is used to signal if login/registration was successful, thus continue through
+       managing the data.
+@return void
+*/
+void adminRegistration(String30 password, String30 tempPassword, String30 currentPassword, bool *returnMenu, bool *bContinue)
+{
     bool validInput = false;
     bool validInput2 = false;
 
@@ -95,6 +167,8 @@ void adminRegistration(String30 password, bool *returnMenu)
             system("cls");
 
             validInput = true;
+            strcpy(currentPassword, password);
+            *bContinue = true;
         }
 
         else if (strcmp(password, tempPassword) != 0)
@@ -129,29 +203,112 @@ void adminRegistration(String30 password, bool *returnMenu)
     } while (validInput == false);
 }
 
-/* This function is the menu for admins which manages the data of the program
-Precondition: cInput from int main() must be 'm' or 'M'
-@param returnMenu This is for choosing if the user wishes to return to the 
-       main menu after an incorrect password input.
+/* This function is for adding a record into the quiz.
+Precondition: The length of the password is limited to 30 characters
+@param password This holds the inputted string as a password.
 @return void
 */
-void manageData(bool *returnMenu)
-{
-    String30 password;
+void addRecord(){
 
-    system("cls");
-
-    adminRegistration(password, returnMenu);
-    
     
 }
 
 /* This function is the menu for admins which manages the data of the program
 Precondition: cInput from int main() must be 'm' or 'M'
-@param <name> <param_description>
+@param password This holds the inputted string as a password.
+@param tempPassword is used to compare the password input.
+@param currentPassword is used if user will return to manage data.
+@param returnMenu This is for choosing if the user wishes to return to the
+       main menu after an incorrect password input.
 @return void
 */
-void enterMenu()
+void manageData(String30 password, String30 tempPassword, String30 currentPassword, bool *returnMenu)
+{
+
+    bool bContinue = false;
+    bool switchValid = false;
+    char adminInput;
+
+    system("cls");
+
+    if (strcmp(password, currentPassword) != 0)
+    {
+        adminRegistration(password, tempPassword, currentPassword, returnMenu, &bContinue);
+    }
+
+    else if (strcmp(password, currentPassword) == 0)
+    {
+
+        adminLogin(password, currentPassword, returnMenu, &bContinue);
+    }
+
+    if (bContinue == true)
+    {
+        while (switchValid = true)
+        {
+
+            system("cls");
+
+            printf("\n[A] to add record");
+            printf("\n[E] to edit record");
+            printf("\n[D] to delete record");
+            printf("\n[I] to import data");
+            printf("\n[X] to export record");
+            printf("\n\n[R] to return to main menu");
+
+            printf("\n\nEnter: ");
+            scanf(" %c", &adminInput);
+
+            switch (adminInput)
+            {
+
+            case 'a':
+            case 'A':
+                // function
+                switchValid = true;
+                break;
+
+            case 'e':
+            case 'E':
+                // function
+                switchValid = true;
+                break;
+
+            case 'd':
+            case 'D':
+                // function
+                switchValid = true;
+                break;
+
+            case 'i':
+            case 'I':
+                // function
+                switchValid = true;
+                break;
+
+            case 'x':
+            case 'X':
+                // function
+                switchValid = true;
+                break;
+
+            case 'r':
+            case 'R':
+                *returnMenu = true;
+                switchValid = true;
+                break;
+            }
+        }
+    }
+}
+
+/* This function serves as the main menu of the program.
+@param password This holds the inputted string as a password.
+@param tempPassword is used to compare the password input.
+@param currentPassword is used if user will return to manage data.
+@return void
+*/
+void enterMenu(String30 password, String30 tempPassword, String30 currentPassword)
 {
 
     char cInput;
@@ -182,7 +339,7 @@ void enterMenu()
         case 'M':
             validInput = true;
             returnMenu = false;
-            manageData(&returnMenu);
+            manageData(password, tempPassword, currentPassword, &returnMenu);
             break;
 
         default:
@@ -194,7 +351,13 @@ void enterMenu()
 int main()
 {
 
-    enterMenu();
+    String30 password;
+    String30 tempPassword;
+
+    // This will change once user registers.
+    String30 currentPassword = "N/A";
+
+    enterMenu(password, tempPassword, currentPassword);
 
     return 0;
 }
